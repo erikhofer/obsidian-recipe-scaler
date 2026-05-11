caeabb479d6f94ebe333005628948c165b1d1b0e
Erik Hofer <git@erikhofer.de>
2026-05-11 16:43:02 +0200

    feat: wire post-processor with note-only scope resolver

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { Plugin } from "obsidian";
 import { RecipeScalerSettings, DEFAULT_SETTINGS } from "./settings/types";
 import { RecipeScalerSettingTab } from "./settings/settings-tab";
+import { ScalerRegistry } from "./registry/scaler-registry";
+import { createPostProcessor } from "./render/post-processor";
 
 export default class RecipeScalerPlugin extends Plugin {
   settings!: RecipeScalerSettings;
+  registry!: ScalerRegistry;
 
   async onload() {
     await this.loadSettings();
+    this.registry = new ScalerRegistry(() => this.settings.pluralPairs);
+
+    this.registerMarkdownPostProcessor(
+      createPostProcessor({
+        registry: this.registry,
+        // Note-only scope for now; canvas resolver added in Task 14.
+        resolveScope: (sourcePath: string) => `note:${sourcePath}`,
+      })
+    );
+
     this.addSettingTab(new RecipeScalerSettingTab(this.app, this));
   }
 
